683fa86b8f48c789b1c64c043ca8f50bdddd668f
moustafaamindev <dev.moustafa.amin@gmail.com>
2023-03-02 13:23:50 +0200

    start project

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import "../scss/app.scss";
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import "bootstrap/dist/js/bootstrap.bundle";
+
+console.log('mostafa amin');
+
+() => {
+    console.log("hi is mostafa webpack")
+};
